4153700e13b175068264ffa67000c2390afca8cc
ivanka <xivankax@gmail.com>
2023-11-05 18:20:17 +0100

    zadnja

--- src/child/type/index.ts
+++ src/child/type/index.ts
@@ -1,10 +1,8 @@
 export class Child {
-    [x: string]: unknown;
-    constructor(
-      public id: string,
-      public firstname: string,
-      public lastname: string,
-      public partentId: string,
-    ) {}
-  }
-  
+  constructor(
+    public id: string,
+    public firstname: string,
+    public lastname: string,
+    public partentId: string,
+  ) {}
+}
